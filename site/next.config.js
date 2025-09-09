bf6ff7fe0f486b75b9be1b6980af1d4e37cff2a9
yudi4090 <yudiasrori760@gmail.com>
2025-09-09 18:03:56 +0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
+    name: 'Kodes registry',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
